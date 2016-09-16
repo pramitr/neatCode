ee922a49337eb9c533e17e5f86d3284e0eb19630
pramitr <pramitroy@gmail.com>
2016-09-16 01:18:10 -0700

    Update addTwoNumbers.java

--- java/addTwoNumbers.java
+++ java/addTwoNumbers.java
@@ -5,6 +5,7 @@
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  * Output: 7 -> 0 -> 8
  * * * * * * * * * * * * * * * * 
+ * 
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -21,9 +22,9 @@ public class Solution {
     public ListNode calculate(ListNode l1, ListNode l2, int carry){
         if(l1 == null & l2 == null && carry == 0) return null;
         if(l1 == null & l2 == null && carry != 0) return new ListNode(1);
-        int number = ((l1==null)?0:l1.val) + ((l2==null)?0:l2.val) + carry;
-        ListNode result = new ListNode(number%10);
-        result.next = calculate((l1==null)?null:l1.next, (l2==null)?null:l2.next, number/10);
+        int sum = ((l1==null)?0:l1.val) + ((l2==null)?0:l2.val) + carry;
+        ListNode result = new ListNode(sum%10); // getting last digit
+        result.next = calculate((l1==null)?null:l1.next, (l2==null)?null:l2.next, sum/10); //if the sum is two digit number first digit is carry
         return result;
     }
 }
