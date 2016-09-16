28a303dad6ca71bee20f2066548d2707da967fd7
Pramit Roy <pramitr@yahoo-inc.com>
2016-09-16 01:13:08 -0700

    Adding length of last word

---
+++ java/lengthOfLastWord.java
@@ -0,0 +1,40 @@
+/**
+
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example, 
+Given s = "Hello World",
+return 5
+
+
+
+*/
+
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int i,l;
+        l = s.length();
+        i = 0;
+        ArrayList<Character> lastWord = new ArrayList<Character>();
+        boolean word = false;
+        while(i<l){
+            if(s.charAt(i) == ' '){
+                word = true;
+            }
+            else {
+                if(word == true){
+                    lastWord = new ArrayList<Character>();
+                    word = false;
+                }
+                lastWord.add(s.charAt(i));
+            }
+            i++;
+        }
+        return lastWord.size();
+    }
+}
